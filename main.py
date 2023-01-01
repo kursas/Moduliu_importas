dd26f3abe663e221d45533d430074ba2d70c2591
kursas <121509033+kursas@users.noreply.github.com>
2023-01-01 19:46:44 +0200

    Create main.py
    # -*- coding: utf-8 -*-
    # Sukurti naują projektą (PyCharm programoje) Jame sukurti failą modulis.py, kuriame:
    # • Kintamajam kintamasis priskirti reikšmę „Čia yra kintamasis“
    # • Sukurti funkciją funkcija(), kuri atspausdina „Čia yra funkcija“.
    # • Sukurti klasę Objektas, kurį iniciavus atspausdina „Čia yra klasė“.
    # Sukurti failą main.py, kuriame:
    # • Importuoti modulį modulis
    # • Atspausdinti importuotą kintamąjį kintamasis
    # • Paleisti importuotą funkciją funkcija()
    # • Inicijuoti importuotos klasės Objektas() objektą

---
+++ main.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Sukurti naują projektą (PyCharm programoje) Jame sukurti failą modulis.py, kuriame:
+# • Kintamajam kintamasis priskirti reikšmę „Čia yra kintamasis“
+# • Sukurti funkciją funkcija(), kuri atspausdina „Čia yra funkcija“.
+# • Sukurti klasę Objektas, kurį iniciavus atspausdina „Čia yra klasė“.
+# Sukurti failą main.py, kuriame:
+# • Importuoti modulį modulis
+# • Atspausdinti importuotą kintamąjį kintamasis
+# • Paleisti importuotą funkciją funkcija()
+# • Inicijuoti importuotos klasės Objektas() objektą
+
+import modulis
+print(modulis.variable)
+modulis.printing_function()
+object=modulis.Object("Class")
+print(object)
+
+#output
+This is variable
+Here is a function
+Where is a Class
+
+Process finished with exit code 0
